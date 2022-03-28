ccd91d779de1f7500e94a998002ed07dfd8dc317
Goncalo <goncaloagnunes@gmail.com>
2022-03-28 14:36:46 +0100

    Asynchronous commit

---
+++ Week6/AysncCakeStarter/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AysncCake
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my birthday party");
+            await HaveAPartyAsync();
+            Console.WriteLine("Thanks for a lovely party");
+
+            CleanUp();
+            Console.ReadLine();            
+        }
+
+        private static async Task HaveAPartyAsync()
+        {
+            var name = "Cathy";
+            var cakeTask = BakeCakeAsync();
+            PlayPartyGames();
+            OpenPresents();
+            var cake = await cakeTask;
+            SingHappyBdayAsync();                          
+            Console.WriteLine($"Happy birthday, {name}, {cake}!!");
+        }
+
+        private static async Task<Cake> BakeCakeAsync()
+        {
+            Console.WriteLine("Cake is in the oven");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Console.WriteLine("Cake is done");
+            return new Cake();
+        }
+
+        private static void PlayPartyGames()
+        {
+            Console.WriteLine("Starting games");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine("Finishing Games");
+        }
+
+        private static void OpenPresents()
+        {
+            Console.WriteLine("Opening Presents");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine("Finished Opening Presents");
+        }
+
+        private static async Task SingHappyBdayAsync()
+        {
+            Console.WriteLine("Everyone gather around. Let's sing together");
+            await Task.Delay(TimeSpan.FromSeconds(2));            
+        }
+
+        private static async Task CleanUp()
+        {
+            Console.WriteLine("Now it's time to clean!");
+            Console.WriteLine("What a mess");
+            await Task.Delay(TimeSpan.FromMinutes(45));
+        }
+    }
+
+    public class Cake
+    {
+        public override string ToString()
+        {
+            return "Here's a cake";
+        }
+    }
+}
