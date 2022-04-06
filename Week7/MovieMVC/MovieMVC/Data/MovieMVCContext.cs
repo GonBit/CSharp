35e41ad3db46cf8236b0abc3455065a8a5220c1b
Goncalo <goncaloagnunes@gmail.com>
2022-04-06 23:03:26 +0100

    MovieMVC

---
+++ Week7/MovieMVC/MovieMVC/Data/MovieMVCContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MovieMVC.Data
+{
+    public class MovieMVCContext : DbContext
+    {
+        public MovieMVCContext (DbContextOptions<MovieMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
